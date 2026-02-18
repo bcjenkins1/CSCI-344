9ad05624b3772d7280c1a1ed72af0364964f6aa5
bcjenkins1 <bjenkin2@unca.edu>
2026-02-18 16:47:56 -0500

    Exam 1 Complete

---
+++ lectures/lecture09/02-style-property-demo/main.js
@@ -0,0 +1,33 @@
+const changeBodyColor = (newColor, selector) => {
+let bodyEl = document.querySelector("selector");
+bodyEl.style.backgroundColor = newColor;  
+}
+
+/**
+
+const makeRed = () => {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('body').style.backgroundColor = 'red';
+};
+
+const makeBlue = () => {
+    console.log('Change background to blue');
+    document.querySelector('body').style.backgroundColor = 'blue'
+};
+
+const makePink = () => {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector('body').style.backgroundColor = 'pink'
+
+};
+
+const makeOrange = () => {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('body').style.backgroundColor = 'orange'
+
+};
+
+ */
