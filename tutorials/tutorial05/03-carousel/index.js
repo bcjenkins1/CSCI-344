c815a62759c0beb0a41906b330492a5247dac2e5
bcjenkins1 <bjenkin2@unca.edu>
2026-02-20 15:20:44 -0500

    Tutorial 5C

--- tutorials/tutorial05/03-carousel/index.js
+++ tutorials/tutorial05/03-carousel/index.js
@@ -2,25 +2,25 @@ let currentPosition = 0;
 let gap = 10;
 const slideWidth = 400;
 
-function moveCarousel(direction) {
-    const items = document.querySelectorAll(".carousel-item");
+function moveCarousel(direction) {  //specify intended direction
+    const items = document.querySelectorAll(".carousel-item");  //set items equal to "carousel-item"
 
-    if (direction == "forward") {
+    if (direction == "forward") {  //if forward
         // minus 2 b/c first 2 slides already showing
-        if (currentPosition >= items.length - 2) {
-            return false;
+        if (currentPosition >= items.length - 2) {  //if at end
+            return false;   //early termination
         }
-        currentPosition++;
-    } else {
-        if (currentPosition == 0) {
-            return false;
+        currentPosition++;  //increase current position
+    } else {                        //otherwise
+        if (currentPosition == 0) {  //if at begining
+            return false;       //early termination
         }
-        currentPosition--;
+        currentPosition--;  //decrease current position
     }
 
-    const offset = (slideWidth + gap) * currentPosition;
+    const offset = (slideWidth + gap) * currentPosition;  //track move formula
 
     for (const item of items) {
-        item.style.transform = `translateX(-${offset}px)`;
+        item.style.transform = `translateX(-${offset}px)`;  //implementing track move function via X position
     }
 }
