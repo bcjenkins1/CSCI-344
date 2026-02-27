a5fc9b441e51e60dc295457922487bfd9dda1b90
bcjenkins1 <bjenkin2@unca.edu>
2026-02-27 14:47:46 -0500

    tutorial 6b complete

--- tutorials/tutorial06/02-item-list/script.js
+++ tutorials/tutorial06/02-item-list/script.js
@@ -1,6 +1,12 @@
 const items= ['Apple', 'Banana', 'Orange', 'Grape', 'Mango'];
-const _____ = document.querySelector("#itemList")
+const itemListEl = document.querySelector("#itemList");
 
 function displayItems() {
-
+itemListEl.innerHTML = "";
+for(let i = 0; i < items.length; i++){
+    const itemstr = `<li>${items[i] }</li>`;
+    itemListEl.insertAdjacentHTML(`beforeend`,itemstr);
+    }
 }
+
+displayItems();
---
+++ tutorials/tutorial06/03-product-catalog/script.js
@@ -0,0 +1,13 @@
+const products = [
+    {name: , price: , description: , category: , inStock: }
+    {name: , price: , description: , category: , inStock: }
+    {name: , price: , description: , category: , inStock: }
+];
+
+function formatPrice(price){ //make 9999.9999 turn into "$9999.99""
+//toFixed(2) dunno how to use this
+}
+
+function createProductCard(//product object){
+    
+}
