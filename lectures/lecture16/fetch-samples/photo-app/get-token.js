741734bffa9476e7c5d79160290d2c515d90a29c
bcjenkins1 <bjenkin2@unca.edu>
2026-03-19 04:45:08 -0400

    Tutorial 7c

---
+++ lectures/lecture16/fetch-samples/photo-app/get-token.js
@@ -0,0 +1,28 @@
+export async function getAccessToken(rootURL, username, password) {
+    const postData = {
+        username: username,
+        password: password,
+    };
+    const endpoint = `${rootURL}/api/token/`;
+    const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+    });
+    const data = await response.json();
+    return data.access_token;
+}
+
+async function printToken() {
+const token = getAccessToken(
+    'https://photo-app-secured.herokuapp.com',
+    'bjenkin2',
+    'password'
+);
+
+console.log(token);
+}
+
+printToken();
--- tutorials/tutorial07/03-course-search/main.js
+++ tutorials/tutorial07/03-course-search/main.js
@@ -5,9 +5,7 @@ let openOnly = false;
 function isClassFull(course) {
     if(course.Classification.Open === false)
         return true;
-    //alternative
-    //return !course.Classification.Open;
-}
+    }
 
 function doesTermMatch(course) {//video 4, but only the first two points are shown
     // If searchTerm is empty, return true (show all courses)
@@ -20,28 +18,33 @@ function doesTermMatch(course) {//video 4, but only the first two points are sho
 
     // Check if searchTerm appears in (all converted to lowercase):
     //   - course.Code
+        const codeMatch = course.Code.includes(searchTerm);
     //   - course.Title
-        if(course.Title.toLowerCase().startsWith(searchTerm)) {
-            return true;
-        } else {
-            return false;
-        }
+    //    if(course.Title.toLowerCase().includes(searchTerm)) {
+    //        return true;
+    //    } else {
+    //        return false;
+    //    }
+        const titleMatch = course.Title.toLowerCase().includes(searchTerm);
     //   - course.CRN (convert to string first)
+        const crnMatch = course.CRN.toString().includes(searchTerm);
     //   - course.Instructors[].Name (use map to get all names, then join)
+        const instructorsMatch = course.Instructors[0].Name.toLowerCase().includes(searchTerm);
     // Use includes() for case-insensitive matching
     // Return true if searchTerm matches any of these fields
-    return true;
+    return codeMatch || titleMatch || crnMatch || instructorsMatch;
 }
 
 function dataToHTML(course) {
     // should return a formatted HTML card with the relevant course info
     // (using template literals). 
+    console.log(course);
     return `
         <section class="course-card">
             <h2>${course.Code}: ${course.Title}</h2>
             <p class="status open">
                 <i class="fa-solid fa-circle-check"></i>
-                Open &bull; 10320 &bull; Seats Available: 14
+                Open &bull; ${course.CRN} &bull; Seats Available: ${course.EnrollmentMax-course.EnrollmentCurrent}
             </p>
             <p>
                 ${course.Days} &bull;
@@ -49,7 +52,7 @@ function dataToHTML(course) {
                 ${course.Hours} Credit Hour(s)
             </p>
             <p>
-                <strong>${1+1}</strong> //getInstructorString(course) video 5 7:47
+                <strong>${course.Instructors[0].Name}</strong>
             </p>
         </section>
     `;
