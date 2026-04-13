6ead78b3d37c4b33fdb10060814b54821464552e
bcjenkins1 <bjenkin2@unca.edu>
2026-04-13 14:01:46 -0400

    hw 4 stuff

---
+++ homework/hw04/src/components/Posts.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { getDataFromServer } from "../server-requests";
+import Post from"./Post.jsx";
+
+export default function Posts({ token }) {
+    const [posts, setPosts] = useState([]);
+
+    async function getPosts() {
+        const data = await getDataFromServer(token, "/api/posts");
+        console.log(data);
+        setPosts(data);
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    return (
+        <div>
+        {
+            posts.map( post => <Post post={post} />)
+        }
+        </div>
+    );
+}
--- tutorials/tutorial09/src/main.jsx
+++ tutorials/tutorial09/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "antd/dist/reset.css";
 
 function main() {
     const rootEl = document.getElementById("app");
