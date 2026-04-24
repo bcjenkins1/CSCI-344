540b26edaa344135beaf61ef37b41e8c776c35de
bcjenkins1 <bjenkin2@unca.edu>
2026-04-24 15:46:11 -0400

    BarChart Complete

--- final-project/frontend/src/components/BarChart.jsx
+++ final-project/frontend/src/components/BarChart.jsx
@@ -12,7 +12,9 @@ import {
 
 export default function BarChartComponent({ items }) {
 
-  const data = items.map(item=>{
+  const sortItems = items.toSorted((a,b)=>a.id-b.id);
+
+  const data = sortItems.map(item=>{
     return {
       name: item.title, value: item.total_units_sold
     }
@@ -29,10 +31,33 @@ export default function BarChartComponent({ items }) {
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis />
-            <Tooltip />
+            <Tooltip content={CustomTooltip}/>
             <Bar dataKey="value" fill="#2f6fed" />
         </BarChart>
         </ResponsiveContainer>
     </div>
 );
 }
+
+function CustomTooltip({ payload, label, active }) {
+  if (active && payload && payload.length) {
+    return (
+      <div
+        className="custom-tooltip"
+        style={{
+          border: '1px solid #d88488',
+          backgroundColor: '#fff',
+          padding: '10px',
+          borderRadius: '5px',
+          boxShadow: '1px 1px 2px #d88488',
+        }}
+      >
+        <p className="label" style={{ margin: '0', fontWeight: '700' }}>{`${label} : ${payload[0].value} Million Units`}</p>
+        <p className="intro" style={{ margin: '0' }}>
+        </p>
+        <p className="desc" style={{ margin: '0', borderTop: '1px dashed #f5f5f5' }}>
+        </p>
+      </div>
+    );
+  }
+  }
